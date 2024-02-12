642cb5137dd0b276fa3f6f02604efe22cc124730
Quiatus <Quiatus@gmail.com>
2024-02-12 16:22:59 +0100

    js fix

--- script/main.js
+++ script/main.js
@@ -1,7 +1,10 @@
-import { converThousand } from './modules/utilities.js';
-import * as qSelectors from './modules/querySelectors.js';
+const btnNextMonth = document.querySelector('#nextMonth');
+const prgNextMonth = document.querySelector('#nextMonthProgress');
+const txtCurrentMonth = document.querySelector('#currentMonth');
 
 let currentMonth = 0;
+
+const converThousand = (string) => string.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
 document.addEventListener('readystatechange', (event) => {
     if (event.target.readyState === "complete") {
@@ -10,24 +13,24 @@ document.addEventListener('readystatechange', (event) => {
 });
 
 const initApp = () => {
-    qSelectors.btnNextMonth.addEventListener('click', nextMonth);
+    btnNextMonth.addEventListener('click', nextMonth);
 }
 
 const moveMonthBar = (progress) => {
     if (progress <= 100) {
-        qSelectors.prgNextMonth.style.background = `radial-gradient(closest-side, #080000 79%, transparent 80% 100%), conic-gradient(#771111 ${progress}%, #200000 0)`;
+        prgNextMonth.style.background = `radial-gradient(closest-side, #080000 79%, transparent 80% 100%), conic-gradient(#771111 ${progress}%, #200000 0)`;
         progress += 2;
         setTimeout(() => moveMonthBar(progress), 10);
     } else {
-        qSelectors.btnNextMonth.classList.toggle('disabledRadial');
-        qSelectors.btnNextMonth.addEventListener('click', nextMonth);
+        btnNextMonth.classList.toggle('disabledRadial');
+        btnNextMonth.addEventListener('click', nextMonth);
     }
 }
 
 const nextMonth = () => {
-    qSelectors.btnNextMonth.classList.toggle('disabledRadial');
+    btnNextMonth.classList.toggle('disabledRadial');
     currentMonth += 1;
-    qSelectors.txtCurrentMonth.textContent = converThousand(currentMonth);
+    txtCurrentMonth.textContent = converThousand(currentMonth);
     moveMonthBar(0);
-    qSelectors.btnNextMonth.removeEventListener('click', nextMonth);
+    btnNextMonth.removeEventListener('click', nextMonth);
 }
